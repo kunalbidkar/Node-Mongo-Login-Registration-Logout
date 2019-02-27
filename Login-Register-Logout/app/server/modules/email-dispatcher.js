f6f1ecf4104253615f3351d39e6781f2b729757b
Kunal Bidkar <kunalbidkar13@gmail.com>
2019-02-26 18:48:55 -0800

    Add files via upload

---
+++ Login-Register-Logout/app/server/modules/email-dispatcher.js
@@ -0,0 +1,34 @@
+
+var EM = {};
+module.exports = EM;
+
+EM.server = require("emailjs/email").server.connect(
+{
+	host 	    : process.env.NL_EMAIL_HOST || 'smtp.gmail.com',
+	user 	    : process.env.NL_EMAIL_USER || 'kunalbidkar13@gmail.com',
+	password    : process.env.NL_EMAIL_PASS || 'my_password',
+	ssl		    : true
+});
+
+EM.dispatchResetPasswordLink = function(account, callback)
+{
+	EM.server.send({
+		from         : process.env.NL_EMAIL_FROM || 'Kunal <do-not-reply@gmail.com>',
+		to           : account.email,
+		subject      : 'Password Reset',
+		text         : 'something went wrong... :(',
+		attachment   : EM.composeEmail(account)
+	}, callback );
+}
+
+EM.composeEmail = function(o)
+{
+	let baseurl = process.env.NL_SITE_URL || 'http://localhost:3000';
+	var html = "<html><body>";
+		html += "Hi "+o.name+",<br><br>";
+		html += "Your username is <b>"+o.user+"</b><br><br>";
+		html += "<a href='"+baseurl+'/reset-password?key='+o.passKey+"'>Click here to reset your password</a><br><br>";
+		html += "Cheers,<br>";
+		html += "</body></html>";
+	return [{data:html, alternative:true}];
+}
---
+++ Login-Register-Logout/readme.md
@@ -0,0 +1,8 @@
+In order to run the project, you need to run these two commands-
+
+npm install
+npm start
+
+
+Go to the browser and run 
+localhost:3000
